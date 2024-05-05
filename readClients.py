3d2fe679896990c53221d373499097342ae241ac
Frances0397 <74672214+Frances0397@users.noreply.github.com>
2024-05-05 21:45:03 +0200

    finished script for importing customer names

--- readClients.py
+++ readClients.py
@@ -1,4 +1,23 @@
 import pandas as pd
+from dotenv import load_dotenv
+import os
+import psycopg2
+
+def connect_to_postgres():
+    try:
+        load_dotenv()
+        # Connect to PostgreSQL
+        conn = psycopg2.connect(
+            host=os.getenv('PGHOST'),
+            database=os.getenv('PGDATABASE'),
+            user=os.getenv('PGUSER'),
+            password=os.getenv('PGPASSWORD'),
+        )
+        print("Connected to PostgreSQL!")
+        return conn
+    except Exception as e:
+        print("Unable to connect to PostgreSQL:", e)
+        return None
 
 def read_excel_file(file_path):
     try:
@@ -19,6 +38,31 @@ if __name__ == "__main__":
     if data is not None:
         print("Data read successfully:")
         cleaned_data = data.dropna()
-        selected_columns = cleaned_data[['Codice', 'Ragione Sociale']]  # Replace 'Column1', 'Column2' with your desired column names
+        selected_columns = data[['Ragione Sociale']]  # Replace 'Column1', 'Column2' with your desired column names
+        selected_rows = selected_columns[selected_columns['Ragione Sociale'].isna() == False]
         print("\nSelected columns:")
         print(selected_columns)
+        print("\nSelected rows")
+        print(selected_rows)
+        
+    conn = connect_to_postgres()
+    df = pd.read_sql_query("SELECT * FROM customers", conn)
+    print(df)
+    
+    for row in selected_rows.iterrows():
+        rag_sociale = row[1]['Ragione Sociale']
+        sql_query = f"INSERT INTO customers (nome) VALUES ('{rag_sociale}') ON CONFLICT (nome) DO NOTHING"
+        print(sql_query)
+        
+        try:
+            with conn.cursor() as cursor:
+                cursor.execute(sql_query)
+                print("Insertion successful!")
+        except Exception as e:
+            print("Error executing SQL query:", e)
+        
+# Commit the transaction
+conn.commit()
+
+# Close the database connection
+conn.close()
